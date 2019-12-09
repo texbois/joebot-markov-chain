5aa004a29ae88fc02b66b0a0d36840223e82fe83
timlathy <tim.labushev@gmail.com>
2019-12-09 09:00:02 +0300

    begin writing a custom chain implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,86 @@
-use pyo3::prelude::*;
-use pyo3::types::PyDict;
+use indexmap::set::IndexSet;
+use std::collections::HashMap;
+use std::convert::TryInto;
+use vkopt_message_parser::reader::{fold_html, EventResult, MessageEvent};
 
-const MARKOVIFY_SETUP: &'static str = r#"
-model = markovify.Text.from_json(model_json)
-def generate(sentence_len):
-    text = None
-    while text is None:
-        text = model.make_short_sentence(sentence_len)
-    return text
-"#;
+const NGRAM_CNT: usize = 2; // Use a bigram markov chain model
 
-pub struct MarkovChain {
-    globals: PyObject,
+type ChainPrefix = [u32; NGRAM_CNT]; // indexes into MarkovChain.words
+
+#[derive(Debug)]
+struct ChainEdge {
+    author_idx: u32,
+    suffix_word_idx: u32,
+}
+
+#[derive(Default, Debug, PartialEq, Eq, Hash)]
+struct MessageAuthor {
+    full_name: String,
+    short_name: String,
+}
+
+#[derive(Default, Debug)]
+struct MarkovChain {
+    words: IndexSet<String>,
+    authors: IndexSet<MessageAuthor>,
+    nodes: HashMap<ChainPrefix, Vec<ChainEdge>>,
+}
+
+#[derive(Default)]
+struct ExtractedMessage {
+    author: MessageAuthor,
+    date: String,
+    body: String,
 }
 
 impl MarkovChain {
-    pub fn create(model_path: &str) -> PyResult<Self> {
-        let gil = Python::acquire_gil();
-        let py = gil.python();
-
-        let model = std::fs::read_to_string(model_path)?;
-
-        let globals_dict = PyDict::new(py);
-        globals_dict.set_item("markovify", py.import("markovify")?)?;
-        globals_dict.set_item("model_json", model)?;
-
-        py.run(MARKOVIFY_SETUP, Some(&globals_dict), None)?;
-
-        let globals = globals_dict.to_object(py);
-        Ok(Self { globals })
+    pub fn build_from_message_dump(input_file: &str) -> Self {
+        let mut chain: Self = Default::default();
+        let _ = fold_html(
+            input_file,
+            Default::default(),
+            |mut msg: ExtractedMessage, event| match event {
+                MessageEvent::FullNameExtracted(full_name) => {
+                    msg.author.full_name.push_str(full_name);
+                    EventResult::Consumed(msg)
+                }
+                MessageEvent::ShortNameExtracted(short_name) => {
+                    msg.author.short_name.push_str(short_name);
+                    EventResult::Consumed(msg)
+                }
+                MessageEvent::DateExtracted(date) => {
+                    msg.date.push_str(date);
+                    EventResult::Consumed(msg)
+                }
+                MessageEvent::BodyExtracted(body) => {
+                    msg.body = body;
+                    append_message(&mut chain, msg);
+                    EventResult::Consumed(Default::default())
+                }
+                _ => EventResult::Consumed(msg),
+            },
+        )
+        .unwrap();
+        chain
     }
+}
 
-    pub fn make_sentence(&self, len: u32) -> PyResult<String> {
-        let gil = Python::acquire_gil();
-        let py = gil.python();
-        let globals = Some(self.globals.extract(py)?);
-        py.eval(&format!("generate({})", len), globals, None)?
-            .extract()
+fn append_message(chain: &mut MarkovChain, message: ExtractedMessage) {
+    let author_idx = chain.authors.insert_full(message.author).0 as u32;
+    let word_indexes = message
+        .body
+        .split(&[' ', '\n'][..])
+        .map(|word| chain.words.insert_full(word.to_owned()).0 as u32)
+        .collect::<Vec<_>>();
+
+    for ngram in word_indexes.windows(NGRAM_CNT + 1) {
+        let (prefix_words, suffix) = ngram.split_at(NGRAM_CNT);
+        let prefix: ChainPrefix = prefix_words.try_into().unwrap();
+        let node = chain.nodes.entry(prefix).or_insert(Vec::new());
+        node.push(ChainEdge {
+            author_idx,
+            suffix_word_idx: suffix[0],
+        });
     }
 }
 
@@ -45,21 +89,39 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_invalid_filename() {
-        assert!(MarkovChain::create("modell.json").is_err());
+    fn test_authors() {
+        let chain = MarkovChain::build_from_message_dump("tests/fixtures/messages.html");
+        assert_eq!(
+            chain.authors.get_index(0),
+            Some(&MessageAuthor {
+                full_name: "Sota Sota".into(),
+                short_name: "sota".into()
+            })
+        );
+        assert_eq!(
+            chain.authors.get_index(1),
+            Some(&MessageAuthor {
+                full_name: "Denko Denko".into(),
+                short_name: "denko".into()
+            })
+        );
+        println!("{:#?}", chain);
     }
 
     #[test]
-    fn test_make_sentence() {
-        let text = MarkovChain::create("model.json").and_then(|chain| chain.make_sentence(40));
-        match text {
-            Ok(text) => assert!(text.len() > 0),
-            Err(err) => {
-                let gil = Python::acquire_gil();
-                let py = gil.python();
-                err.print(py);
-                panic!()
-            }
-        }
+    fn test_words_order() {
+        let chain = MarkovChain::build_from_message_dump("tests/fixtures/messages.html");
+        assert_eq!(chain.words.get_index(0), Some(&"Привет".into()));
+        assert_eq!(chain.words.get_index(1), Some(&"Denko".into()));
+    }
+
+    #[test]
+    fn test_no_empty_words() {
+        let chain = MarkovChain::build_from_message_dump("tests/fixtures/messages.html");
+        let enumerated_words = chain.words.iter().enumerate();
+        let empty_words =
+            enumerated_words.filter_map(|(i, w)| if w.is_empty() { Some(i) } else { None });
+
+        assert_eq!(vec![0usize; 0], empty_words.collect::<Vec<_>>());
     }
 }
