442b0c14ef4762862fe3b578152014016adfffba
timlathy <tim.labushev@gmail.com>
2020-01-08 17:24:39 +0300

    create an example of creating a chain and serializing it to a json file

---
+++ README.md
@@ -0,0 +1,4 @@
+# Joebot Markov Chain
+
+* Creating a chain from text files and VkOpt message dumps: see `examples/mkchain.rs`
+* Using the chain to generate random messages: see unit tests in `src/lib.rs`
---
+++ examples/mkchain.rs
@@ -0,0 +1,14 @@
+use joebot_markov_chain::chain::{Datestamp, MarkovChain};
+use serde_json;
+use std::fs::File;
+
+fn main() {
+    let mut chain = MarkovChain::new();
+    chain.append_message_dump("tests/fixtures/messages.html");
+    chain.append_text(
+        "tests/fixtures/text",
+        vec!["text".into()],
+        Datestamp { year: 2020, day: 0 },
+    );
+    serde_json::to_writer(&File::create("chain.json").unwrap(), &chain).unwrap();
+}
