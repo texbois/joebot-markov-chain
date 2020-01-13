7e6965c5dd42086bca9ca0a32284e95c0d014529
timlathy <tim.labushev@gmail.com>
2020-01-13 11:14:41 +0300

    serialize chain using bincode (faster & occupies less space)

--- examples/mkchain.rs
+++ examples/mkchain.rs
@@ -1,14 +1,19 @@
 use joebot_markov_chain::{ChainAppend, Datestamp, MarkovChain};
-use serde_json;
+use bincode;
 use std::fs::File;
 
 fn main() {
     let mut chain = MarkovChain::new();
-    chain.append_message_dump("tests/fixtures/messages.html");
+    chain.append_message_dump("messages.html");
     chain.append_text(
         "tests/fixtures/text",
         vec!["text".into()],
         Datestamp { year: 2020, day: 0 },
     );
-    serde_json::to_writer(&File::create("chain.json").unwrap(), &chain).unwrap();
+    println!(
+        "Chain entries mem allocation: {} bytes",
+        chain.sources.iter().map(|s| s.entries.capacity()).sum::<usize>()
+            * std::mem::size_of::<joebot_markov_chain::ChainEntry>()
+    );
+    bincode::serialize_into(&File::create("chain.bin").unwrap(), &chain).unwrap();
 }
